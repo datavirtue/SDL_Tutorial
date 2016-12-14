9952b089b256f303253d547c87399bb7319339f3
sean.anderson <sean.anderson@datavirtue.com>
2016-12-13 23:49:43 -0500

    added comments based on my understanding of the code

--- src/CApp.cpp
+++ src/CApp.cpp
@@ -6,7 +6,8 @@ CApp::CApp(){
     Running = true;
 }
 
-
+const int SCREEN_WIDTH = 640;
+    const int SCREEN_HEIGHT = 480;
 
 int CApp::OnExecute(){
     if(OnInit() == false){
@@ -15,12 +16,23 @@ int CApp::OnExecute(){
 
     SDL_Event Event;
 
+    /* The game loop */
     while(Running){
+        /*
+            SDL wraps OS event handling.  We pass in an address
+            (handled as a pointer in the SDL code) for an SDL Event
+            variable and the loop executes for each event in the queue
+            populating that variable with event data.  We use that event
+            data to make decisions and branch our code in the OnEvent method.
+        */
         while(SDL_PollEvent(&Event)){
             OnEvent(&Event);
         }
+
         OnLoop();
+
         OnRender();
+
     }
 
     OnCleanup();
--- src/CApp_OnEvent.cpp
+++ src/CApp_OnEvent.cpp
@@ -1,8 +1,12 @@
 #include "..\include\CApp.h"
 
 void CApp::OnEvent(SDL_Event *Event){
+
+    /* SDL_QUIT event type is triggered when we X out a window! */
     if(Event->type == SDL_QUIT){
 
         Running = false;
     }
+
+
 }
